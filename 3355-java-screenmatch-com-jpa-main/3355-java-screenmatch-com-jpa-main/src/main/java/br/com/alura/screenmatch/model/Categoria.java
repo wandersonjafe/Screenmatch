1f48ab4a27c5ead8dbb07f520f1c264134fdd332
Wanderson Jafé <wandersonjafe@hotmail.com>
2025-07-21 03:35:31 -0300

    Primeiro commit: adicionando projeto Screenmatch

---
+++ 3355-java-screenmatch-com-jpa-main/3355-java-screenmatch-com-jpa-main/src/main/java/br/com/alura/screenmatch/model/Categoria.java
@@ -0,0 +1,37 @@
+package br.com.alura.screenmatch.model;
+
+public enum Categoria {
+    ACAO("Action", "Ação"),
+    AVENTURA("Adventure", "Aventura"),
+    ROMANCE("Romance", "Romance"),
+    COMEDIA("Comedy", "Comédia"),
+    DRAMA("Drama", "Drama"),
+    CRIME("Crime", "Crime"),
+    TERROR("Horror", "Terror");
+
+    private final String categoriaOmdb;
+    private String categoriaPortugues;
+
+    Categoria(String categoriaOmdb, String categoriaPortugues) {
+        this.categoriaOmdb = categoriaOmdb;
+        this.categoriaPortugues = categoriaPortugues;
+    }
+
+    public static Categoria fromString(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaOmdb.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+
+    public static Categoria fromPortugues(String text) {
+        for (Categoria categoria : Categoria.values()) {
+            if (categoria.categoriaPortugues.equalsIgnoreCase(text)) {
+                return categoria;
+            }
+        }
+        throw new IllegalArgumentException("Nenhuma categoria encontrada para a string fornecida: " + text);
+    }
+}
